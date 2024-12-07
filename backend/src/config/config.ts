450b6f9cfcbc7d7305f2a52e2010cfd9c04a467d
Marc <marc@marc>
2024-12-06 19:48:37 -0500

    adjustments

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -4,6 +4,7 @@ export const config = {
       "password": process.env.POSTGRES_PASSWORD,
       "database": process.env.POSTGRES_DB,
       "host": process.env.POSTGRES_HOST,
+      "postgres_disable_ssl": process.env.POSTGRES_DISABLE_SSL ?? false,
       "aws_region": process.env.AWS_REGION,
       "cognito_user_pool_id": process.env.COGNITO_USER_POOL_ID,
       "token_use": process.env.TOKEN_USE,
--- backend/src/sequelize.ts
+++ backend/src/sequelize.ts
@@ -1,19 +1,31 @@
-import { Sequelize } from 'sequelize-typescript';
+import { Sequelize, SequelizeOptions } from 'sequelize-typescript';
 import { config } from './config/config.js';
 import { Trade } from './models/trade.js'
 
 const c = config.dev;
 
-// Instantiate new Sequelize instance!
-export const sequelize = new Sequelize({
+let seq: SequelizeOptions = {
   "username": c.username,
   "password": c.password,
   "database": c.database,
   "host":     c.host,
 
   dialect: 'postgres',
-  storage: ':memory:',
-});
+  storage: ':memory:'
+};
+
+if(c.postgres_disable_ssl === false)
+{
+  seq.dialectOptions = {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+   }
+  };
+}
+
+// Instantiate new Sequelize instance!
+export const sequelize = new Sequelize(seq);
 
 export async function syncModels() {
   sequelize.addModels([Trade])
